d05f082b821f46e01b3bb88171d9915ecdccb352
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2025-04-29 19:28:27 -0400

    Update ReviewAnalysis.java

--- ReviewAnalysis.java
+++ ReviewAnalysis.java
@@ -6,7 +6,11 @@ public class ReviewAnalysis {
     allReviews = reviews;
   }
   public double getAverageRating () {
-     
+     double sum=0;
+    for (Review r: allReviews) {
+      sum+=r.getRating();
+    } 
+    return sum/allReviews.length;
   }
   public ArrayList <String> collectComments() {
     
