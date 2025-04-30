1f3421f15a4348c413bb2a9d66747d9beaff700b
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2025-04-29 21:50:33 -0400

    Update ReviewAnalysis.java

--- ReviewAnalysis.java
+++ ReviewAnalysis.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class ReviewAnalysis {
   private Review [] allReviews; 
   public ReviewAnalysis () {
@@ -13,6 +14,15 @@ public class ReviewAnalysis {
     return sum/allReviews.length;
   }
   public ArrayList <String> collectComments() {
-    
+    ArrayList <String> comments = new ArrayList<String> (); 
+    for (int i = 0; i<allReviews.length; i++) {
+      String s= i +"-" + allReviews[i].getcomment();
+      String last = s.substring(s.length()-1);
+      if (!last.equals(".") && !last.equals("!") {
+        s+=".";
+    }
   }
+    return comments;
+}
+
 }
